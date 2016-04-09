12cc510a73df23567815e01fe19bc401f29b5618
SABRINA TEE <sabrinatee@me.com>
2016-04-09 21:22:48 +0800

    Added node modules

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = module.exports = exports = express();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/kitty_app_dev');
+
+const kittyRouter = require(__dirname + '/app/routes/kitty_routes');
+const userRouter = require(__dirname + '/app/routes/user_routes');
+
+app.use('/api', kittyRouter);
+app.use('/api', userRouter);
+
+app.use(express.static(__dirname + '/build'));
+
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('server up on port: ' + PORT));
